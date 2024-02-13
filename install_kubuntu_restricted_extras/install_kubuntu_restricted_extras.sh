af22658a1b712c722a11ce814bce59a7aefec64c
xbrncsr <cesarbrunoms@gmail.com>
2024-02-13 13:52:41 -0400

    update

--- install_kubuntu_restricted_extras/install_kubuntu_restricted_extras.sh
+++ install_kubuntu_restricted_extras/install_kubuntu_restricted_extras.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Install Ubuntu Restricted Extras
-sudo apt install -y ubuntu-restricted-extras
+sudo apt install -y kubuntu-restricted-extras
 
 #
 sudo apt install -y fonts-roboto fonts-cascadia-code fonts-firacode
 
 # Compressed files support
-sudo apt install -y unace rar unrar zip unzip p7zip-full p7zip-rar
+sudo apt-get -y install unace rar unrar zip unzip p7zip-full p7zip-rar
