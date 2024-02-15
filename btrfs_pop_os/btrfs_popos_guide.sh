cef8d0864e8b4dc4052b6ab3475ae5522d8d703e
xbrncsr <cesarbrunoms@gmail.com>
2024-02-15 16:09:03 -0400

    UPDATE

--- btrfs_pop_os/btrfs_popos_guide.sh
+++ btrfs_pop_os/btrfs_popos_guide.sh
@@ -22,3 +22,84 @@ sudo cryptsetup luksClose /dev/mapper/data-root
 sudo cryptsetup luksClose /dev/mapper/cryptdata
 ls /dev/mapper
 
+# ===================== NÃO REINICIE O LIVE CD
+
+# ===================== O vídeo mesmo começa 10:40min
+
+# (OPCIONAL)caso tenha criptografado o disco
+cryptsetup luksOpen /dev/sda3 cryptdata
+
+# ===================== 01
+# Verificar status
+ls /dev/mapper
+# Monte o sistema de arquivos raiz de nível superior btrfs com compactação zstd
+mount -o subvolid=5,defaults,compress=zstd:1,discard=async /dev/mapper/data-root /mnt
+
+# ===================== 02
+# verificar status
+btrfs subvolume list /mnt
+# Criando btrfs subvolumes @ and @home
+btrfs subvolume create /mnt/@
+cd /mnt
+ls | grep -v @ | xargs mv -t @
+ls -a /mnt
+
+btrfs subvolume create /mnt/@home
+mv /mnt/@/home/* /mnt/@home/
+ls -a /mnt/@/home
+ls -a /mnt/@home
+
+btrfs subvolume list /mnt
+
+# ===================== 03
+# Changes to fstab
+sed -i 's/btrfs  defaults/btrfs  defaults,subvol=@,compress=zstd:1,discard=async/' /mnt/@/etc/fstab
+echo "UUID=$(blkid -s UUID -o value /dev/mapper/data-root)  /home  btrfs  defaults,subvol=@home,compress=zstd:1,discard=async   0 0" >> /mnt/@/etc/fstab
+
+# (OPCIONAL) Changes to crypttab
+sed -i 's/luks/luks,discard/' /mnt/@/etc/crypttab
+cat /mnt/@/etc/crypttab
+# cryptdata UUID=52d31097-e125-46f0-a139-85087e1b5565 none luks,discard
+# cryptswap UUID=8cd56bf1-1a43-49c3-98b0-835b658b54fc /dev/urandom swap,plain,offset=1024,cipher=aes-xts-plain64,size=512
+
+# ===================== 04
+# Adjust configuration of kernelstub
+nano /mnt/@/etc/kernelstub/configuration
+# logo abaixo de: "splash", <== não esqueça da virgula após "splash"
+# acrescente: "rootflags=subvol=@"
+
+# ===================== 05
+# Adjust configuration of systemd bootloader
+# Troque: sda1
+mount /dev/sda1 /mnt/@/boot/efi
+sed -i 's/splash/splash rootflags=subvol=@/' /mnt/@/boot/efi/loader/entries/Pop_OS-current.conf
+cat /mnt/@/boot/efi/loader/entries/Pop_OS-current.conf
+
+# ===================== 06
+# Create a chroot environment and update initramfs
+cd /
+umount -l /mnt
+mount -o subvol=@,defaults,compress=zstd:1,discard=async /dev/mapper/data-root /mnt
+ls /mnt
+
+# Then the following commands will put us into our system using chroot (taken from System76’s help post on how to Repair the Bootloader):
+for i in /dev /dev/pts /proc /sys /run; do mount -B $i /mnt$i; done
+chroot /mnt
+
+# We are now inside the new system, so let’s check whether our fstab mounts everything correctly:
+mount -av
+
+# Looks good! Now we need to update the initramfs to make it aware of our changes to the kernelstub:
+update-initramfs -c -k all
+
+# ===================== Reboot o live cd
+
+# Verifique se esta tudo ok
+sudo mount -av
+sudo mount -v | grep /dev/mapper
+sudo swapon
+sudo btrfs subvolume list /
+
+# Ativar o trim para SSD
+sudo systemctl enable fstrim.timer
+
