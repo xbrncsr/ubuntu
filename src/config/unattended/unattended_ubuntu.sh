7703d9d3a0e36aacf2351aba553c4c3876726f6d
xbrncsr <cesarbrunoms@gmail.com>
2024-02-17 07:48:14 -0400

    add unattended

---
+++ src/config/unattended/unattended_ubuntu.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#
+# SOURCE:
+# <https://youtu.be/o0z8UjU0AJs?si=TcX1mSedEC8hwFrw>
+# <https://tonyteaches.tech/ubuntu-automatic-update-tutorial/>
+#
+
+# Instala o pacote unattended-upgrades
+sudo apt install -y unattended-upgrades update-notifier-common
+
+# Atualiza a configuração do Unattended Upgrades
+sudo nano /etc/apt/apt.conf.d/50unattended-upgrades
+
+# Certifique-se de que as seguintes linhas estejam presentes e descomentadas
+sudo sed -i '/^\/\/Unattended-Upgrade::Remove-Unused-Kernel-Packages/s/^\/\///' /etc/apt/apt.conf.d/50unattended-upgrades
+sudo sed -i '/^\/\/Unattended-Upgrade::Remove-New-Unused-Dependencies/s/^\/\///' /etc/apt/apt.conf.d/50unattended-upgrades
+sudo sed -i '/^\/\/Unattended-Upgrade::Remove-Unused-Dependencies/s/^\/\///' /etc/apt/apt.conf.d/50unattended-upgrades
+sudo sed -i '/^\/\/Unattended-Upgrade::Automatic-Reboot/s/^\/\///' /etc/apt/apt.conf.d/50unattended-upgrades
+
+# Definir horário do reboot
+//Unattended-Upgrade::Automatic-Reboot-Time "02:00";
+
+# Para aplicar as configurações
+sudo dpkg-reconfigure -plow unattended-upgrades
+sudo unattended-upgrades --dry-run --debug
