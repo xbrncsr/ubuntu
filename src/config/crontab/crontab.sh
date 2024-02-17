54d90bed49261c0701343db7ed30b7c71f17d0cb
xbrncsr <cesarbrunoms@gmail.com>
2024-02-17 11:38:26 -0400

    bugfix

--- src/config/crontab/crontab.sh
+++ src/config/crontab/crontab.sh
@@ -2,5 +2,5 @@
 
 # Crontab
 # $i: Indica ao sed para inserir a linha antes da última linha.
-sudo sed -i '$i 25 10 * * * root sudo apt update && sudo apt upgrade -y && sudo apt full-upgrade -y' /etc/crontab
+sudo sed -i '$i 40 10 * * * root sudo apt update && sudo apt upgrade -y && sudo apt full-upgrade -y' /etc/crontab
 
