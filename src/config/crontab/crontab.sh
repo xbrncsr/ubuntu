eb66d1e1d27f8431aec11bff23897eb77ae611e0
xbrncsr <cesarbrunoms@gmail.com>
2024-02-17 16:21:24 -0400

    update bugfix

--- src/config/crontab/crontab.sh
+++ src/config/crontab/crontab.sh
@@ -9,10 +9,10 @@
 #
 # ========== EXEMPLO MANUAL EDITANDO CRONTAB
 # sudo nano /etc/crontab
-# 35  15  *   *   *   root    sudo apt update && sudo apt upgrade -y && sudo apt full-upgrade -y
+# 25 16 * * *     root    sudo apt update && sudo apt upgrade -y && sudo apt full-upgrade -y && sudo snap refresh && sudo reboot now
 #
 # ========== OU COM SCRIPT
-# 35  15  *   *   *   root    /home/user/documents/scripts/auto_update.sh
+# 25 16 * * *     root    /home/user/documents/scripts/auto_update.sh
 #
 # ========== NÃO ESQUEÇA DAS PERMISSÕES DO SCRIPT
 # chmod 777 auto_update.sh
@@ -22,4 +22,4 @@
 #
 # ========== EXEMPLO AUTOMATICO
 # $a: Indica ao sed para inserir na ultima linha.
-sudo sed -i '$a 10  16  *   *   *   root    sudo apt update && sudo apt upgrade -y && sudo apt full-upgrade -y && sudo snap refresh && sudo reboot now' /etc/crontab
+sudo sed -i '$a 25 16 * * *     root    sudo apt update && sudo apt upgrade -y && sudo apt full-upgrade -y && sudo snap refresh && sudo reboot now' /etc/crontab
