b52c2e2f31e97ef93a130863cd41521595194b02
xbrncsr <cesarbrunoms@gmail.com>
2024-02-17 15:59:36 -0400

    update

--- src/config/crontab/crontab.sh
+++ src/config/crontab/crontab.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
 
 #
-# Status Crontab
+# ========== STATUS CRONTAB
 # /etc/init.d/cron status
+#
 
-# Editando Crontab
+
+#
+# ========== EXEMPLO MANUAL EDITANDO CRONTAB
 # sudo nano /etc/crontab
+# 35  15  *   *   *   root    sudo apt update && sudo apt upgrade -y && sudo apt full-upgrade -y
+#
+# ========== OU COM SCRIPT
+# 35  15  *   *   *   root    /home/user/documents/scripts/auto_update.sh
+#
+# ========== NÃO ESQUEÇA DAS PERMISSÕES DO SCRIPT
+# chmod 777 auto_update.sh
+#
 
+
+#
+# ========== EXEMPLO AUTOMATICO
 # $a: Indica ao sed para inserir na ultima linha.
-sudo sed -i '$a 35  15  *   *   *   root    sudo apt update && sudo apt upgrade -y && sudo apt full-upgrade -y' /etc/crontab
+sudo sed -i '$a 10  16  *   *   *   root    sudo apt update && sudo apt upgrade -y && sudo apt full-upgrade -y && sudo reboot now' /etc/crontab
