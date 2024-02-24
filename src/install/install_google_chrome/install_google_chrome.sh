a109bffe490bcc686a7d1e2eb230fab901a92a2c
xbrncsr <cesarbrunoms@gmail.com>
2024-02-24 09:44:31 -0400

    update comando

--- src/install/install_google_chrome/install_google_chrome.sh
+++ src/install/install_google_chrome/install_google_chrome.sh
@@ -6,7 +6,7 @@
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 
 # Instalando
-sudo dpkg -i google-chrome-stable_current_amd64.deb
+sudo apt install ./google-chrome-stable_current_amd64.deb
 
 # Removendo pacote
 rm google-chrome-stable_current_amd64.deb
